f8d555e483fb2a94b30965325c2bbd619f4994ec
Baymax14 <85451508+Baymax14@users.noreply.github.com>
2021-08-03 13:24:49 +0530

    Add files via upload

---
+++ ThA-Day-16/Readme.md
@@ -0,0 +1,1 @@
+https://csb-t97sj-bj96ttd3o-baymax14.vercel.app/
---
+++ ThA-Day-16/src/components/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+  return(
+      <div className="card">
+          <img src="https://toppng.com/uploads/preview/orang-transparent-orange-meme-11563225464xcvkdkjoic.png" alt="" />
+          <h1>React card</h1>
+          <p>This is a meme card</p>
+      </div>
+  );
+};
+
+export default Card;
