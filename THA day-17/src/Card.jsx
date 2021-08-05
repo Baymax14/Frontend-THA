a5ebd1d1cb05680782717f8a32c93526e906cbe2
Baymax14 <85451508+Baymax14@users.noreply.github.com>
2021-08-05 11:37:55 +0530

    Add files via upload

---
+++ THA day-17/src/Card.jsx
@@ -0,0 +1,11 @@
+const Card = () => {
+  return (
+    <div>
+      <img src="" alt=""/>
+      <div className="Title">Title</div>
+      <button>Button</button>
+    </div>
+  );
+
+};
+export default Card;
