2590b414f8594c626d84fa83fc74f72df5a20562
Baymax14 <85451508+Baymax14@users.noreply.github.com>
2021-08-09 18:39:05 +0530

    Add files via upload

---
+++ day 24/day 24/src/Components/About.js
@@ -0,0 +1,4 @@
+import "./style.css";
+export default function Home() {
+  return <div className="cmn">contact us</div>;
+}
---
+++ day 24/day 24/src/Components/Home.js
@@ -0,0 +1,9 @@
+import "./style.css";
+export default function Home() {
+	return(
+<div className="cmn">
+
+Browse from the safety of your home
+</div>
+	)
+}
